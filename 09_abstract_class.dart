69ad89df2b9b48f06884d7df925204358ca56408
caegomezda <camiloedg23@gmail.com>
2026-01-16 16:04:51 -0500

    09 Class abstract class 2

--- 09_abstract_class.dart
+++ 09_abstract_class.dart
@@ -1,5 +1,17 @@
 void main (){
-//   final windPlant = EnergyPlant();
+  final windPlant = WindPlant( initialEnergy: 100);
+  
+  print(windPlant);
+  
+  print("wind ${ chargePhone( windPlant ) }" );
+}
+
+double chargePhone ( EnergyPlant plant ){
+  if ( plant.energyLeft < 10){
+    throw Exception("Not enough energy");
+  }
+  
+  return plant.energyLeft - 10;
 }
 
 enum PlantType { nuclear, wind, water}
@@ -14,4 +26,14 @@ abstract class EnergyPlant {
   });
   
   void consumeEnergy( double amount );
+}
+
+//extends or implements
+class WindPlant extends EnergyPlant{
+  WindPlant({ required double initialEnergy })
+    :super( energyLeft: initialEnergy, type: PlantType.wind );
+  @override
+  void consumeEnergy ( double amount ){
+    energyLeft -= amount;
+  }
 }
